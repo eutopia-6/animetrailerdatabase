d1814f89a87ec7c06c805b984c939039871e6348
Summit <lisummit6@gmail.com>
2023-06-21 01:51:05 -0400

    -Fixed issue so sidebar will also now bring user to trailer page

--- animetrailers/src/components/Sidebar.js
+++ animetrailers/src/components/Sidebar.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Sidebar({topAnime}) {
   return (
     <aside>
         <nav>
             <h3>Top Anime</h3>
-            {topAnime.map(anime => (
-            <a
-                href = {anime.trailer.embed_url}
-                target = "_blank"
-                key={anime.mal_id}
-                rel = "noreferrer">
-                {anime.title}
-            </a>
-            ))}
+            {topAnime.map(anime => {
+                // const animeURL = anime.trailer.embed_url;
+                // const animeTitle = anime.title;
+                // const synopsis = anime.synopsis;
+                const uniqueId = anime.mal_id;
+                
+                return <Link to={`/trailers/${uniqueId}`} 
+                state={{ animeURL:anime.trailer.embed_url, 
+                        animeTitle:anime.title, 
+                        synopsis:anime.synopsis}}>
+                    {anime.title}
+                </Link>
+            })}
+       
             
             
         </nav>
